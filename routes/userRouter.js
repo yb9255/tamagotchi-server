c623023413f84b67a7541b0ab8244c77fa52cc73
yb9255 <yoobin9255@gmail.com>
2022-07-10 20:27:01 +0900

    사용자 정보 업데이트 로직

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,12 +2,14 @@ const express = require('express');
 const {
   postLogin,
   getUserInformation,
+  patchUserInformation,
 } = require('../controllers/user.controller');
 const { verifyToken, isLoggedIn } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/login', verifyToken, postLogin);
-router.get('/userInformation', isLoggedIn, getUserInformation);
+router.get('/user-information', isLoggedIn, getUserInformation);
+router.patch('/new-information', isLoggedIn, patchUserInformation);
 
 module.exports = router;
