a874f5806c06c945345de3467c50e6f80ac63147
yb9255 <yoobin9255@gmail.com>
2022-07-14 20:50:21 +0900

    client url cors 허용

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: [process.env.ENV === 'development' && 'http://localhost:3000'],
+    origin: [
+      process.env.ENV === 'development' && 'http://localhost:3000',
+      'https://yb-web-tamagotchi.netlify.app/',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   }),
